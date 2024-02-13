b61003f092eb6ae4143053f7570b2d1014d5c691
Maximo Hernandez <maximojhernandezla@gmail.com>
2024-02-13 13:13:16 -0300

    add: prisma, user and pet schema, dbConnection, routes: test routes

--- be-huella-project/src/app.js
+++ be-huella-project/src/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import config from './config/dotenv.js';
 import cookieParser from 'cookie-parser';
-
+import userTestRouter from './routes/testRoutes/db-test.user.router.js';
+import petTestRouter from './routes/testRoutes/db-test.pet.router.js';
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use('/api/user-test', userTestRouter);
+app.use('/api/pet-test', petTestRouter);
 
 app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
