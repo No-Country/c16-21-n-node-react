836e39c65ad5e5cb2638b54dbdb24e9f8c597316
luiggy <luiggylml@gmail.com>
2024-02-14 17:42:15 -0500

    test: prueba de pr

--- fe-huella-project/src/App.tsx
+++ fe-huella-project/src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
       <h1>App Huellas</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore repellendus adipisci, aspernatur magni vitae molestias ipsum neque hic quis tempore?</p>
     </div>
   )
 }
