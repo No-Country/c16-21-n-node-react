a04042715febd2550374c9d35559839dd9831b83
Tomas Juarez <mokajua@gmail.com>
2024-02-14 19:22:39 -0300

    reac-router intallation / pages / components

--- fe-huella-project/src/main.tsx
+++ fe-huella-project/src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { Home } from './pages/Home.tsx';
+
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createHashRouter([
+
+  {
+    path: "/",
+    element: <Home/>
+  },
+
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ fe-huella-project/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from "../components/Navbar"
+
+
+export const  Home = () => {
+  return (
+    <div className="">
+    
+    <Navbar/>
+
+    <h1 className="">huellApp Home</h1>
+    
+    </div>
+    
+  )
+}
