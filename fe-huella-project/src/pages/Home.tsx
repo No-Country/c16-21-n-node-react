74b477e06e2528368b3902e88c7edcd2735bfd00
luiggy <luiggylml@gmail.com>
2024-02-15 19:02:37 -0500

    test: prueba de pr

--- fe-huella-project/src/pages/Home.tsx
+++ fe-huella-project/src/pages/Home.tsx
@@ -6,6 +6,7 @@ export const  Home = () => {
     <div className="">
     <Navbar/>
     <h1 className="">huellApp Home</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates dicta facilis repellat at similique pariatur doloribus. Hic non in obcaecati.</p>
     </div>
     
   )
