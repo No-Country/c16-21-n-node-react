ec574545283eb01845c3253d58df52d74a868793
maxjhernandez <maximojhernandezla@gmail.com>
2024-02-16 18:00:04 -0300

    Revert "Be/feature/max/documentation"

--- be-huella-project/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const __mainDirname = path.join(__dirname, '..');
-
-export { __mainDirname };
