9b1612e4f455c1b45f2b5809331e294ce1278df4
Tomas Juarez <mokajua@gmail.com>
2024-02-22 19:35:41 -0300

    fix

--- fe-huella-project/src/pages/Home.tsx
+++ fe-huella-project/src/pages/Home.tsx
@@ -1,11 +1,10 @@
-import { Navbar } from "../components/Navbar"
 
-export const  Home = () => {
+
+export const Home = () => {
 
   return (
     <div className="">
-    <Navbar/>
-    <h1 className="">huellApp Home</h1>
+   
     </div>
     
   )
