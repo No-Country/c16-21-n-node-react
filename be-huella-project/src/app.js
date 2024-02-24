4f4991a173da421fa9baa6df665e1e4ec8be5ad6
Mauricio Cruz Diaz <macrudi@gmail.com>
2024-02-24 13:13:56 -0500

    Add dao

--- be-huella-project/src/app.js
+++ be-huella-project/src/app.js
@@ -5,6 +5,7 @@ import userTestRouter from './routes/testRoutes/db-test.users.router.js';
 import petTestRouter from './routes/testRoutes/db-test.pets.router.js';
 import jwtTestRouter from './routes/testRoutes/db-test.jwt.router.js';
 import jwtRouter from './routes/jwt.router.js';
+import usersRouter from './routes/users.router.js';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpecs } from './utils/swagger.js';
 import { middlewareErrorHandler } from './errors/error-handler.js';
@@ -20,6 +21,7 @@ app.use('/api/users-test', userTestRouter);
 app.use('/api/pets-test', petTestRouter);
 app.use('/api/token-test', jwtTestRouter);
 app.use('/api/users/login', jwtRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middlewareErrorHandler);
 app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
