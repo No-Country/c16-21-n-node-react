59072157d0bf52d5d7a18f13377e2a007f79843f
Maximo Hernandez <maximojhernandezla@gmail.com>
2024-02-26 21:42:32 -0300

    intentando fusionar ramas

--- be-huella-project/src/firebase/firebase.js
+++ be-huella-project/src/firebase/firebase.js
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore'; // Importa la función getFirestore
 
 // Tu configuración de Firebase
+=======
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+>>>>>>> 0718d42 (firebase added)
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_APIKEY,
   authDomain: process.env.FIREBASE_AUTHDOMAIN,
@@ -11,6 +20,7 @@ const firebaseConfig = {
   appId: process.env.FIREBASE_APPID,
 };
 
+<<<<<<< HEAD
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -18,3 +28,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
+=======
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+>>>>>>> 0718d42 (firebase added)
