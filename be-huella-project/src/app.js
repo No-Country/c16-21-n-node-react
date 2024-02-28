15f5fe92e9ada5965c7beb2c49d550b32389faf4
Mauricio Cruz Diaz <macrudi@gmail.com>
2024-02-28 18:17:10 -0500

    Add CORS

--- be-huella-project/src/app.js
+++ be-huella-project/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import config from './config/dotenv.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import userTestRouter from './routes/testRoutes/db-test.users.router.js';
 import petTestRouter from './routes/testRoutes/db-test.pets.router.js';
 import jwtTestRouter from './routes/testRoutes/db-test.jwt.router.js';
@@ -10,11 +11,13 @@ import petsRouter from './routes/pets.router.js';
 import { swaggerSpecs } from './utils/swagger.js';
 import { middlewareErrorHandler } from './errors/error-handler.js';
 import usersRouter from './routes/users.router.js';
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 app.use('/api/users-test', userTestRouter);
