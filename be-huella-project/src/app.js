5d955a2b4d151cdaa868e4106255f4052adc8da4
Maximo Hernandez <maximojhernandezla@gmail.com>
2024-02-28 21:54:00 -0300

    muchos cambios hechos

--- be-huella-project/src/app.js
+++ be-huella-project/src/app.js
@@ -4,7 +4,6 @@ import cookieParser from 'cookie-parser';
 import userTestRouter from './routes/testRoutes/db-test.users.router.js';
 import petTestRouter from './routes/testRoutes/db-test.pets.router.js';
 import jwtTestRouter from './routes/testRoutes/db-test.jwt.router.js';
-import jwtRouter from './routes/jwt.router.js';
 import swaggerUi from 'swagger-ui-express';
 import petsRouter from './routes/pets.router.js';
 import { swaggerSpecs } from './utils/swagger.js';
@@ -20,7 +19,6 @@ app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 app.use('/api/users-test', userTestRouter);
 app.use('/api/pets-test', petTestRouter);
 app.use('/api/token-test', jwtTestRouter);
-app.use('/api/users/login', jwtRouter);
 
 app.use('/api/users', usersRouter);
 app.use('/api/pets', petsRouter);
--- be-huella-project/src/routes/users.router.js
+++ be-huella-project/src/routes/users.router.js
@@ -6,13 +6,12 @@ import { isAuth } from '../middlewares/isAuth.js';
 const router = Router();
 
 router.post('/recover-password', usersController.recoverPassword);
-router.post('/recover-password/:uid', isAuth, usersController.resetPassword);
-
 router.get('/', usersController.getAllUsers);
 router.put('/update', isAuth, upload, usersController.userUpdate);
 router.delete('/delete', isAuth, usersController.userDelete);
 router.post('/signin', upload, usersController.userCreate);
 router.get('/find', usersController.userFind);
-router.get('/find/:uid', usersController.userFindId);
+router.get('/:uid', isAuth, usersController.userFindId);
+router.post('/login', usersController.login);
 
 export default router;
