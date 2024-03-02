22b3a389cf56c064368a543225545e88979f9760
Mauricio Cruz Diaz <macrudi@gmail.com>
2024-03-02 11:02:39 -0500

    Remove auth to pets filters

--- be-huella-project/src/routes/pets.router.js
+++ be-huella-project/src/routes/pets.router.js
@@ -5,7 +5,7 @@ import { upload } from '../middlewares/multer.js';
 const router = Router();
 
 router.get('/', petsController.getAllPets);
-router.get('/:race?/:type?/:gender?/:location?/:lostOrFound?',  isAuth, petsController.getAllPetsFilter);
+router.get('/:race?/:type?/:gender?/:location?/:lostOrFound?', petsController.getAllPetsFilter);
 router.get('/:pid', isAuth, petsController.getPetById);
 router.post('/create', isAuth, upload, petsController.createPet);
 router.put('/update/:pid', upload, isAuth, petsController.updatePet);
