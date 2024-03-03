05a4dace9ef0670d2a6636dd788848dc44b3c579
lorex03 <lorenaflorio03@gmail.com>
2024-03-03 07:52:50 -0300

    si no esta logueado no puede crear mascota, y en el form create, devuelve el token pero en consola, resolver ultimo detalle del servidor

--- fe-huella-project/src/components/AuthContext.tsx
+++ fe-huella-project/src/components/AuthContext.tsx
@@ -1,17 +1,25 @@
-
 import { createContext, useContext, useState, ReactNode } from "react";
+
+
+
 
 interface User {
   accessToken: string;
-  Token: string;
+
 }
      
 interface AuthContextType {
-  user: User 
+  user: User
   setUser: (user: User) => void;
 }
 
+
+
+
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+
+
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
@@ -20,20 +28,15 @@ export const useAuth = () => {
   }
   return context;
 };
-export const getUserFromAccessToken = (accessToken: string) => {
-  try {
-    const tokenData = accessToken.split('.')[1];
-    const decodedToken = JSON.parse(atob(tokenData));
-    return decodedToken.user;
-  } catch (error) {
-    console.error("Error al decodificar el token: ", error);
-    return null;
-  }
-}
+
+
 
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User>({ accessToken: "",  Token: "" });
+  const [user, setUser] = useState<User>({ accessToken: "" });
+
+
+
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
@@ -41,40 +44,3 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     </AuthContext.Provider>
   );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { createContext, useContext, useState } from "react";
-
-// interface User { name: string; email: string; role: string; }
-
-// interface AuthContextProps { user: User | null; setUser: (user: User | null) => void; }
-
-// const AuthContext = createContext<AuthContextProps>({ user: null, setUser: () => {} });
-
-// export const useAuth = () => { return useContext(AuthContext); };
-
-// export const AuthProvider: React.FC<{children: React.ReactNode}> = ({ children }) => { const [user, setUser] = useState<null | User>(null);
-
-// return ( <AuthContext.Provider value={{ user, setUser }}> {children} </AuthContext.Provider> ); };
-
-// export default AuthProvider;
-
-
-// // En este código estás creando un contexto de autenticación (`AuthContext`) que contiene la información del usuario y una función para establecer el usuario. Luego, creas un custom hook `useAuth` que te permite acceder al contexto de autenticación en cualquier componente.
-
-// // Además, defines un componente `AuthProvider` que actúa como proveedor de contexto y tiene un estado local para almacenar la información del usuario. Este componente proporciona el contexto de autenticación a todos sus hijos a través del `AuthContext.Provider`.
-
-// // En resumen, este código se encarga de manejar la autenticación y proveer la información del usuario a lo largo de la aplicación utilizando React Context API.
--- fe-huella-project/src/pages/CreatePet.tsx
+++ fe-huella-project/src/pages/CreatePet.tsx
@@ -1,13 +1,10 @@
-
-
 import { useForm, SubmitHandler } from "react-hook-form";
 import axios from "axios";
 import { useMutation } from "react-query";
 import { useNavigate } from "react-router-dom";
 import dogCreate from "../assets/Perro create card .png";
 import { useAuth } from "../components/AuthContext";
-import { getUserFromAccessToken } from "../components/AuthContext";
-
+import { useEffect } from "react";
 interface CreateFormValues {
     name: string;
     race: string;
@@ -22,28 +19,28 @@ interface CreateFormValues {
   when: string;
 }
 
-
-
 export const CreatePet: React.FC = () => {
   const { register, handleSubmit } = useForm<CreateFormValues>();
   const navigate = useNavigate();
+  const { user } = useAuth(); // Verificar si el usuario tiene el accessToken al cargar el componente 
+  useEffect(() => { if (!user.accessToken) { navigate("/login"); // Redirigir al usuario a la página de login si no tiene accessToken 
+} }, [user.accessToken , navigate]);
 
 
-  const { user } = useAuth();
-  const authenticatedUser = getUserFromAccessToken(user.accessToken);
-
-  
   const { mutate } = useMutation(
     async (data: CreateFormValues) => {
-      const response = await axios.post("https://apihuellapptest.up.railway.app/api/pets", data, {
+      const response = await axios.post("https://apihuellapptest.up.railway.app/api/pets/create", data, {
         headers: {
             Authorization: `Bearer ${user.accessToken}`
         }
     });
-    
+   
       console.log(response.data);
     }
   );
+
+
+
 
   const onSubmit: SubmitHandler<CreateFormValues> = (data) => {
     mutate(data, {
@@ -57,10 +54,16 @@ export const CreatePet: React.FC = () => {
     });
   };
 
+
+
+
   return (
     <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       <h1 className="text-black font-bold text-6xl py-4" style={{ textAlign: "center" }}>Nueva <span className="text-orange-huella">Mascota!</span></h1>
+
+
+
 
       <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
     <img
@@ -77,46 +80,68 @@ export const CreatePet: React.FC = () => {
     />
 </div>
 
+
+
+
 {/* Subir fotografías en la que el rostro del perro sea visible, hasta X MB */}
+
+
+
 
           <form onSubmit={handleSubmit(onSubmit)} style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "15px", marginTop: "50px" }}>
             <label>Nombre </label>
             <input type="name" placeholder="Nombre de usuario" {...register("name", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
 
 
+
+
+
+
+
+
             <label>race</label>
             <input type="race" placeholder="Teléfono" {...register("race", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
+
+
+
+
+
+
 
 
             <label>type</label>
             <input type="type" placeholder="type" {...register("type", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
 
 
+
+
+
+
+
+
             <label>photo</label>
             <input type="photo" placeholder="imagen photo perfil"  {...register("photo", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
 
-            {/* <button type="submit" className="inline-block m-auto text-center w-[105%] rounded-full h-10 mr-auto shadow-md shadow-gray-400 text-sm px-4 py-3 leading-none text-white bg-blue-buttons hover:border-transparent hover:shadow-none hover:text-blue-buttons hover:bg-white mt-30 lg:mt-0 button-registrar" style={{ marginTop: "15px", textAlign: "center", left: "56px" }}>
-  Registrar como perdido
-</button> */}
 
-    
-{/* position: absolute;
-width: 349.86px;
-height: 380px;
-left: 192px;
-top: 317px;
-
-filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25)); */}
 
 
             <label>Locacion</label>
             <input type="location" placeholder="location" {...register("location", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
-          
+         
             <label>gender</label>
             <input type="gender" placeholder="gender"  {...register("gender", { required: true })}className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
 
+
+
+
             <label>lostOrFound</label>
             <input type="lostOrFound" placeholder="lostOrFound" {...register("lostOrFound", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
+
+
+
+
+
+
 
 
             <label>necklace</label>
@@ -131,15 +156,11 @@ filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25)); */}
             <input type="age" placeholder="age" {...register("age", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
             <label>when</label>
             <input type="date" placeholder="when"  {...register("when", { required: true })}className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
-
-
-
             <button type="submit" className="inline-block m-auto text-center w-[105%] rounded-full h-10 mr-auto shadow-md shadow-gray-400 text-sm px-4 py-3 leading-none text-white bg-blue-buttons hover:border-transparent hover:shadow-none hover:text-blue-buttons hover:bg-white mt-30 lg:mt-0" style={{ marginTop: "15px", textAlign: "center" }}>
               Registrar Perfil
             </button>
           </form>
         </div>
       </div>
-
   );
 };
