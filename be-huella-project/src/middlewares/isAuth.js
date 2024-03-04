d475cfcbda627d1b1babf51dc7dffc51c5793cbb
Maximo Hernandez <maximojhernandezla@gmail.com>
2024-03-04 18:40:55 -0300

    cors modificado

--- be-huella-project/src/middlewares/isAuth.js
+++ be-huella-project/src/middlewares/isAuth.js
@@ -14,3 +14,24 @@ export const isAuth = async (req, res, next) => {
     next(error);
   }
 };
+
+// import * as Errors from '../errors/custom-exeptions.js';
+// import { verifyToken } from '../utils/utils.js';
+
+// export const isAuth = async (req, res, next) => {
+//   try {
+//     const authHeader = req.headers['authorization'];
+//     if (!authHeader || !authHeader.startsWith('Bearer '))
+//       throw new Errors.Unathorized('You need authentication');
+
+//     const token = authHeader.split(' ')[1];
+//     if (!token) throw new Errors.Unathorized('Token not provided');
+
+//     const decodedToken = verifyToken(token);
+//     req.user = decodedToken.user;
+
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// };
