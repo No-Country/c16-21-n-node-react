08fd7474055a3b91208cca95d680a1ff0b4e898c
Tomas Juarez <mokajua@gmail.com>
2024-03-04 22:50:06 -0300

    profile y pet maquetado

--- fe-huella-project/src/routes/AppRouter.tsx
+++ fe-huella-project/src/routes/AppRouter.tsx
@@ -1,51 +1,56 @@
-    import { createHashRouter } from "react-router-dom";
-    import {  Root } from "../pages/Root";
-    import { Register } from "../pages/Register";
-    import { Home } from "../pages/Home";
-    import { Login } from "../pages/Login";
-    import { About } from "../pages/About";
-    import { Error } from "../components/Error";
+import { createHashRouter } from "react-router-dom";
+import { Root } from "../pages/Root";
+import { Register } from "../pages/Register";
+import { Home } from "../pages/Home";
+import { Login } from "../pages/Login";
+import { About } from "../pages/About";
+import { Error } from "../components/Error";
 import { RescuePasword } from "../pages/RescuePasword";
-import {CreatePet} from "../pages/CreatePet.tsx"
+import { CreatePet } from "../pages/CreatePet.tsx";
+import { Userprofile } from "../pages/Userprofile.tsx";
+import { Petprofile } from "../pages/Petprofile.tsx";
 
-
-     export const router = createHashRouter([
-
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <Error />,
+    children: [
       {
-        path: "/",
-        element: <Root />,
-        errorElement: <Error/> ,
-        children: [
-          {
-            element:<Error/>
-          },
-          {
-            index:true,
-            element: <Home />,
-          },
-          {
-            path: "/register",
-            element: <Register/>,
-          },
-          {
-            path:"/login",
-            element:<Login/>
-          },
-          {
-            path:"/about",
-            element:<About/>
-          },
-          {
-            path:"/rescuepassword",
-            element:<RescuePasword/>
-          },
-          {
-            path:"/create",
-            element:<CreatePet/>
-          }
-        ],
+        element: <Error />,
       },
-      
-    ]);
-    
-    
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/rescuepassword",
+        element: <RescuePasword />,
+      },
+      {
+        path: "/create",
+        element: <CreatePet />,
+      },
+      {
+        path: "userprofile",
+        element: <Userprofile />,
+      },
+      {
+        path: "petprofile",
+        element: <Petprofile />,
+      },
+    ],
+  },
+]);
