4f57230dcffd79a25cebe15022e10bf05fe5e3e9
lorex03 <lorenaflorio03@gmail.com>
2024-03-06 04:05:54 -0300

    toma todos los campos, pero todavia tengo q combinar el codigo de la otra parte,para q funcione photo

--- fe-huella-project/src/pages/CreatePet.tsx
+++ fe-huella-project/src/pages/CreatePet.tsx
@@ -7,11 +7,12 @@ import { useAuth } from "../components/AuthContext";
 import { useEffect, useState } from "react";
  import { useDropzone } from 'react-dropzone';
 
+
 interface CreateFormValues {
   name: string;
     race: string;
     type: string;
-    image: string,
+    photo: string,
   location: string;
   gender: string;
   lostOrFound:string;
@@ -22,17 +23,25 @@ interface CreateFormValues {
 }
 export const CreatePet: React.FC = () => {
   const { register ,handleSubmit } = useForm<CreateFormValues>();
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: (acceptedFiles: File[]) => {
+      const file = acceptedFiles[0];
+      console.log(file);
+      // Aquí puedes enviar el archivo al servidor o hacer lo que necesites con el mismo
+    }
+  });
   const navigate = useNavigate();
   const { user } = useAuth();
-  const [uploadedFile, setUploadedFile] = useState<string | null>(null);
-
+ 
    // Verificar si el usuario tiene el accessToken al cargar el componente
   useEffect(() => { if (!user.accessToken) { navigate("/login"); // Redirigir al usuario a la página de login si no tiene accessToken
 } }, [user.accessToken , navigate]);
 
+
 const onSubmit: SubmitHandler<CreateFormValues> =(data) => {
- 
-  const formData = new FormData();
+    const formData = new FormData();
+
+
 
     formData.append("name", data.name);
     formData.append("race", data.race);
@@ -44,48 +53,35 @@ const onSubmit: SubmitHandler<CreateFormValues> =(data) => {
     formData.append("weight", String(data.weight));
     formData.append("age", String(data.age));
     formData.append("when", data.when);
-    formData.append("image", data.image);
+    formData.append("when", data.when);
 
-    axios
-    .post("https://apihuellapptest.up.railway.app/api/pets/create", formData, {
+
+    axios.post("https://apihuellapptest.up.railway.app/api/pets/create", formData, {
       headers: { Authorization: `Bearer ${user.accessToken}` },
     })
-    .then((response) => {
-      console.log("Mascota creada con éxito", response.data);
-      navigate("/");
-    })
-    .catch((error) => {
-      console.error("Error al crear la mascota:", error);
-    });
-};
-
-const onDrop = (acceptedFiles: File[]) => {
-  
-  const file = acceptedFiles[0];
-  const formData = new FormData();
-  
-  formData.append("image", file);
-
-  axios.post('https://apihuellapptest.up.railway.app/api/pets/create', formData,
-  
-  
-  
-  {
-    headers: {
-      Authorization: `Bearer ${user.accessToken}`
-    }
-  })
-    .then((response) => {
-      const imageUrl = response.data.url;
-      setUploadedFile(imageUrl);
-    }).catch((error) => {
-      console.error('Error al subir la imagen: ', error);
-    });
-};
-
-const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
 
+  .then((response) => {
+          const imageUrl = response.data.url; // Obtener la URL de la imagen subida
+          setUploadedFile(imageUrl);
+      }).catch((error) => {
+          console.error('Error al subir la imagen: ', error);
+      });
+  };
+
+ const [uploadedFile, setUploadedFile] = useState('');
+ 
+// const onDrop = (acceptedFiles: File[]) => {
+//   const file = acceptedFiles[0];
+//   const formData = new FormData();
+//   formData.append('image', file);
+//   console.log(File)
+//   //  manejar los archivos que se han subido al dropzone
+//   console.log(acceptedFiles);
+// };
+
+
+// const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
 
   return (
@@ -107,11 +103,7 @@ const { getRootProps, getInputProps } = useDropzone({ onDrop });
     />
 </div>
 
-
-
-
 {/* Subir fotografías en la que el rostro del perro sea visible, hasta X MB */}
-
 
 <form onSubmit={handleSubmit(onSubmit)}
            style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "15px", marginTop: "50px" }}>
@@ -119,10 +111,6 @@ const { getRootProps, getInputProps } = useDropzone({ onDrop });
             <input type="name" placeholder="Nombre de usuario" {...register("name", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
             <label>race</label>
             <input type="race" placeholder="Teléfono" {...register("race", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
-
-
-
-
             <label>type</label>
             <input type="type" placeholder="type" {...register("type", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
          <label>Locacion</label>
@@ -130,37 +118,34 @@ const { getRootProps, getInputProps } = useDropzone({ onDrop });
          
             <label>gender</label>
             <input type="gender" placeholder="gender"  {...register("gender", { required: true })}className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
-
-
             <label>lostOrFound</label>
             <input type="lostOrFound" placeholder="lostOrFound" {...register("lostOrFound", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
             <label>necklace</label>
             <input type="necklace" placeholder="necklace" {...register("necklace", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
 
+
             <label>weight</label>
             <input type="weight" placeholder="weight"  {...register("weight", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
             <label>age</label>
             <input type="age" placeholder="age" {...register("age", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
-       
-
-
-
-
+    
             <label>when</label>
             <input type="date" placeholder="when" {...register("when", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
-           
+   
+            <label>photo</label>
+            <div {...getRootProps()} style={{ border: '1px solid black', padding: '20px', margin: '20px 0' }}>
+        <input {...getInputProps()}         />
+        <p>Arrastra y suelta una imagen aquí, o haz clic para seleccionarla.</p>
+      </div>
+      {uploadedFile && (
+  <div>
+    <img src={uploadedFile} alt="Vista previa de la imagen" style={{ width: "200px", height: "auto" }} />
+  </div>
+)}
 
-
-
-            <div {...getRootProps()} style={{ border: '2px dashed #333', padding: '20px', textAlign: 'center', cursor: 'pointer' }}>
-            Arrastra y suelta una imagen aquí, o haz clic para seleccionar un archivo
-          </div>
-          <label>image</label>
-          <input {...getInputProps()} placeholder="imagen photo mascota" {...register("image", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
-          {uploadedFile && <img src={uploadedFile} alt="uploaded" />}
-
-
-
+            {/* <input type="photo" placeholder="imagen photo perfil"  {...register("photo", { required: true })} className="w-500 h-250 p-15 pl-24 pr-302 rounded-12 border border-gray-400" />
+            */}
+            {/* <label>Imagen de la mascota</label> */}
             <button type="submit" className="inline-block m-auto text-center w-[105%] rounded-full h-10 mr-auto shadow-md shadow-gray-400 text-sm px-4 py-3 leading-none text-white bg-blue-buttons hover:border-transparent hover:shadow-none hover:text-blue-buttons hover:bg-white mt-30 lg:mt-0" style={{ marginTop: "15px", textAlign: "center" }}>
               Registrar Perfil
             </button>
